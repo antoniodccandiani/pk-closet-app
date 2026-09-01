880e8d836c8730b2f8794c4b7e0ab39e8c8dfe6d
antoniodccandiani <antoniodccandiani@gmail.com>
2026-09-01 00:35:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from pathlib import Path
 import sys
 import random
 
-sys.path.insert(0, str(Path(__file__).parent / "src"))
+sys.path.insert(0, str(Path(__file__).parent))
 from composer import create_bom_dia, create_funcionamento, gerar_legenda_produto, create_produto
 
 st.set_page_config(
